838e24daf680d877499a367ffe8f725edc3ea956
PC <PC@192.168.1.10>
2019-04-24 17:48:33 -0300

    Adicionando validacao no form de clientes.

--- Locadora/src/main/java/com/estudo/spring/Locadora/controller/CarroController.java
+++ Locadora/src/main/java/com/estudo/spring/Locadora/controller/CarroController.java
@@ -1,11 +1,19 @@
 package com.estudo.spring.Locadora.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.estudo.spring.Locadora.model.Carro;
 import com.estudo.spring.Locadora.model.Cliente;
@@ -17,7 +25,7 @@ public class CarroController {
 
 	@Autowired
 	private CarroRepository carroRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
 
@@ -52,10 +60,21 @@ public class CarroController {
 	}
 
 	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
-	public String adicionarCliente(@PathVariable("codigo") int codigo, Cliente cliente) {
-		Carro carro = carroRepository.findById(codigo);
-		cliente.setCarro(carro);
-		clienteRepository.save(cliente);
+	public String adicionarCliente(@PathVariable("codigo") int codigo, @Valid Cliente cliente, BindingResult result,
+			RedirectAttributes attributes) {
+		List<String> mensagens = new ArrayList<>();
+		if (result.hasErrors()) {
+			List<ObjectError> errors = result.getAllErrors();
+			for (ObjectError error : errors) {
+				mensagens.add(error.getDefaultMessage());
+			}
+		} else {
+			Carro carro = carroRepository.findById(codigo);
+			cliente.setCarro(carro);
+			clienteRepository.save(cliente);
+			mensagens.add("Cliente adicionado com sucesso!");
+		}
+		attributes.addFlashAttribute("mensagens", mensagens);
 		return "redirect:/{codigo}";
 	}
 
--- Locadora/src/main/java/com/estudo/spring/Locadora/model/Cliente.java
+++ Locadora/src/main/java/com/estudo/spring/Locadora/model/Cliente.java
@@ -3,12 +3,17 @@ package com.estudo.spring.Locadora.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente {
 
 	@Id
+	@NotNull(message = "Campo de rg nao pode estar vazio.")
 	private Integer rg;
+	
+	@Size(min = 2, max = 50, message = "Campo deve conter no minimo 2 caracteres e no maximo 50.")
 	private String nome;
 
 	@ManyToOne
